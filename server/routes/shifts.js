9b893a9e9049d16465043795f68cefef5b50a5f5
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-08-31 22:05:05 +1000

    buld the translink methods and start journeys endpoint

--- server/routes/shifts.js
+++ server/routes/shifts.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import Tanda from '../../lib/tanda';
+import Translink from '../../lib/translink';
 
 const router = new Router();
 const tanda = new Tanda();
+const tl = new Translink(process.env.TRANSLINK_USERNAME, process.env.TRANSLINK_PASSWORD);
 
 router.get('/', (req, res) => {
   if (!req.user) {
@@ -33,6 +35,23 @@ router.get('/', (req, res) => {
       ))
     .then(shifts => Promise.all(shifts))
     .then(shifts => res.status(200).json(shifts));
+});
+
+router.get('/journeys', async (req, res) => {
+  console.log(req.query);
+  const currentLocation = req.query.currentLocation;
+  const orgLocation = req.query.orgLocation;
+  const startTime = req.query.startTime;
+  try {
+    const startLocation = (await tl.getLocationId(currentLocation.latitude,
+      currentLocation.longitude)).Suggestions[0].Id;
+    const endLocation = (await tl.getLocationId(orgLocation.latitude,
+      orgLocation.longitude)).Suggestions[0].Id;
+    const journeys = await tl.getJourneys(startLocation, endLocation, startTime);
+    return res.json(journeys);
+  } catch (e) {
+    return res.json(e);
+  }
 });
 
 export default router;
